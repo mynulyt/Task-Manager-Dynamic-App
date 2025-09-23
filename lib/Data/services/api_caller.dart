d15f252a2b74d5bf513e84731dc39496ec76d72b
mynulyt <mynulalam27@gmail.com>
2025-09-23 21:47:13 +0100

    api caller

---
+++ lib/Data/services/api_caller.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:logger/logger.dart';
+
+class ApiCaller {
+  static final Logger _logger = Logger();
+  static Future<ApiResponse> getRequest({required String url}) async {
+    try {
+      Uri uri = Uri.parse(url);
+
+      _logRequest(url);
+      Response response = await get(uri);
+      _logResponse(url, response);
+
+      final int statusCode = response.statusCode;
+      if (statusCode == 200) {
+        //success
+        final decodeData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: statusCode,
+          responseData: decodeData,
+          errorMessage: '',
+        );
+      } else {
+        //Faild
+        final decodeData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: decodeData,
+          errorMessage: decodeData['data'],
+        );
+      }
+    } on Exception catch (e) {
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: -1,
+        responseData: null,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static Future<ApiResponse> postRequest({
+    required String url,
+    Map<String, dynamic>? body,
+  }) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logRequest(url, body: body);
+      Response response = await post(
+        uri,
+        headers: {'content-type': 'application/json'},
+        body: jsonEncode(body),
+      );
+      _logResponse(url, response);
+      final int statusCode = response.statusCode;
+      if (statusCode == 200 || statusCode == 201) {
+        //success
+        final decodeData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: statusCode,
+          responseData: decodeData,
+          errorMessage: '',
+        );
+      } else {
+        //Faild
+        final decodeData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: decodeData,
+          errorMessage: decodeData['date'],
+        );
+      }
+    } on Exception catch (e) {
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: -1,
+        responseData: null,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static void _logRequest(String url, {Map<String, dynamic>? body}) {
+    _logger.i(
+      'URL => $url\n'
+      'Request Body : $body',
+    );
+  }
+
+  static void _logResponse(String url, Response response) {
+    _logger.i(
+      'URL=> $url\n'
+      'Status Code : ${response.statusCode}\n'
+      'Body : ${response.body}',
+    );
+  }
+}
+
+class ApiResponse {
+  final bool isSuccess;
+  final int responseCode;
+  final dynamic responseData;
+  final String? errorMessage;
+
+  ApiResponse({
+    required this.isSuccess,
+    required this.responseCode,
+    required this.responseData,
+    required this.errorMessage,
+  });
+}
