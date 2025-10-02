ad891e32fcd3a5f584f16dbf756a5038ddd5215b
mynulyt <mynulalam27@gmail.com>
2025-10-02 23:22:24 +0600

    complete task screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:doto_manager/app.dart';
-import 'package:doto_manager/ui/controller/auth_controller.dart';
+
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await AuthController.getUserData();
   runApp(TaskManagerApp());
 }
