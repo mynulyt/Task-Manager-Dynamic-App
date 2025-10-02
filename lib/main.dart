39d48d1082d24c0b42c0945f1325ea0568321b16
mynulyt <mynulalam27@gmail.com>
2025-10-02 23:17:32 +0600

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:doto_manager/app.dart';
+import 'package:doto_manager/ui/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthController.getUserData();
   runApp(TaskManagerApp());
 }
